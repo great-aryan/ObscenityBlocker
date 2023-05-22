d41a533c495285265f9ebdbcacb82bb2a8af1138
Aryan <aryans2808@gmail.com>
2023-05-23 03:23:04 +0530

    Initial commit

---
+++ app/src/main/java/com/digitalrakshak/obscenityblocker/SigninupActivity.kt
@@ -0,0 +1,11 @@
+package com.digitalrakshak.obscenityblocker
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SigninupActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_signinup)
+    }
+}
